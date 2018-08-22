fd0a993d47475025d4413095639935e70c73ed7d
Aditya Kolla <aditya.169108011@muj.manipal.edu>
2018-08-23 01:47:45 +0530

    Added basic login with passport

--- app.js
+++ app.js
@@ -1,20 +1,79 @@
 //SETUP
-const bodyParser = require("body-parser"),
-  mongoose = require("mongoose"),  
-  express = require("express"),
-  app = express();
+const LocalStrategy         = require("passport-local"),
+  bodyParser                = require("body-parser"),
+  passport                  = require('passport'),
+  mongoose                  = require("mongoose"),
+  express                   = require("express"),
+  app                       = express();
 
+const User = require('./models/user');
+
+//CONFIGURE APP
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect('mongodb://localhost:27017/placement_app', { useNewUrlParser: true });
+
+//PASSPORT SETUP
+app.use(require('express-session')({
+  secret: "THIS IS THE BIGGEST SECRET!",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 //ROUTES
 //Direct Route
-app.get("/", (req, res) => {
-  res.render("home");
+app.get("/", isLoggedIn, (req, res) => {
+  res.render("index");
 });
 
-app.post("/login", (req, res) => {
 
+//AUTH ROUTES
+//get signup page
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+//Handling signup logic
+app.post("/signup", (req, res) => {
+  let newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, (err, user) => {
+    if(err){
+      console.log(err);
+      return res.render("signup");
+    }
+    passport.authenticate("local")(req, res, () => {
+      res.redirect("/");
+    });
+  });
+});
+
+//Get the login page
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+//handling login logic
+app.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login"
+}), (req, res) => {
+});
+
+//Handle logout logic
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
 });
 
 //Wrong link
@@ -24,3 +83,11 @@ app.get("*", (req, res) => res.send("Sorry, page not found...What are you doing 
 app.listen(3000, 'localhost', () =>{
     console.log("Server has started");
 });
+
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+}
